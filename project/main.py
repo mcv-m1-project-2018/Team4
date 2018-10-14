a7fb43fe3930108b0ad03cbc76271be222a5235f
manurare <mreyarea@gmail.com>
2018-10-14 15:08:21 +0200

    Change task1 to calculate characteristics

--- project/main.py
+++ project/main.py
@@ -9,7 +9,7 @@ Taking into account that dir train must be at the same level as main.py
 
 import argparse
 from pathlib import Path
-import task1
+import calculate_characteristics
 import task2
 import histogram_analysis_method
 
@@ -18,14 +18,14 @@ if __name__ == "__main__":
 
     ap = argparse.ArgumentParser()
     ap.add_argument("-dp", "--dataset_path", required=False, default="train",
-                    help="Dataset path, by default it should be at the same level as task1.py")
+                    help="Dataset path, by default it should be at the same level as main.py")
 
     args = vars(ap.parse_args())
 
     file_path = Path(__file__).parent.absolute()
     dataset_path = str(file_path / Path(args["dataset_path"]))
     # executing tasks:
-    dataset_grouped, dataset = task1.calculate_characteristics(dataset_path)
+    dataset_grouped, dataset = calculate_characteristics.calculate_characteristics(dataset_path)
     dataset_train, dataset_valid = task2.dataset_split(dataset_grouped, 0.3)
     # The following line is optional, exporting histogram for: 
     # training dataset signs if (dataset, 0)
